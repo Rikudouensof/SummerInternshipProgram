41288c8f6173527fe7feb945b243b5314566902d
Solomon Uke <engineer@rikudouensof.com>
2024-05-15 03:09:12 +0100

    LogHelper Dependency Injection

--- SummerInternshipProgram.API/Program.cs
+++ SummerInternshipProgram.API/Program.cs
@@ -1,15 +1,37 @@
 
+using NLog.Extensions.Logging;
+using SummerInternshipProgram.API.Helpers.Implementation;
+using SummerInternshipProgram.API.Helpers.Interface;
+
 namespace SummerInternshipProgram.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+
+            NLogProviderOptions nlpopts = new NLogProviderOptions
+            {
+                IgnoreEmptyEventId = true,
+                CaptureMessageTemplates = true,
+                CaptureMessageProperties = true,
+                ParseMessageTemplates = true,
+                IncludeScopes = true,
+                ShutdownOnDispose = true
+            };
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+
+
+            //Dependency Injection of Services and Helpers
+            builder.Services.AddScoped<ILogHelper, LogHelper>();
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
