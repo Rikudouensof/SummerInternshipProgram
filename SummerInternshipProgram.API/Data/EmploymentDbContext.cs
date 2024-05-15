e305d5c91f7283daf728961dc644876785b6d780
Solomon Uke <engineer@rikudouensof.com>
2024-05-15 04:26:20 +0100

    Add Db Context

---
+++ SummerInternshipProgram.API/Data/EmploymentDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SummerInternshipProgram.API.Models.DatabaseModels;
+
+namespace SummerInternshipProgram.API.Data
+{
+    public class EmploymentDbContext : DbContext
+    {
+
+        public DbSet<Applicant>? Applicants { get; set; }
+        public DbSet<DateQuestion>? DateQuestions { get; set; }
+        public DbSet<DropdownQuestion>? DropdownQuestions { get; set; }
+        public DbSet<Gender>? Genders { get; set; }
+        public DbSet<MultipleChoiceQuestion>? MultipleChoiceQuestions { get; set; }
+        public DbSet<NumericQuestion>? NumericQuestions { get; set; }
+        public DbSet<ParagraphQuestion>? ParagraphQuestions { get; set; }
+        public DbSet<Program>? Programs { get; set; }
+        public DbSet<QuestionType>? QuestionTypes { get; set; }
+        public DbSet<YesOrNoQuestion>? YesOrNoQuestions { get; set; }
+
+
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+
+
+}
